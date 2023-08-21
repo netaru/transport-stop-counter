6a5d31afdabcdcd24fd6c081d16f5555eb094697
David Jonsson <david.jonsson306@gmail.com>
2023-08-21 14:44:04 +0200

    [Service] Added configuration for url and key

--- transport-stop-counter-service/src/main/java/se/david/TransportStopCounterServiceConfiguration.java
+++ transport-stop-counter-service/src/main/java/se/david/TransportStopCounterServiceConfiguration.java
@@ -1,8 +1,39 @@
 package se.david;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.dropwizard.core.Configuration;
+import jakarta.validation.constraints.NotEmpty;
 
 public class TransportStopCounterServiceConfiguration extends Configuration
 {
-    // TODO: implement service configuration
+    @NotEmpty
+    private String backendUrl;
+
+    @NotEmpty
+    private String slKey;
+
+    @JsonProperty
+    public String getBackendUrl()
+    {
+        return backendUrl;
+    }
+
+    @JsonProperty
+    public void setBackendUrl(String backendUrl)
+    {
+        this.backendUrl = backendUrl;
+    }
+
+    @JsonProperty
+    public String getSlKey()
+    {
+        return slKey;
+    }
+
+    @JsonProperty
+    public void setSlKey(String slKey)
+    {
+        this.slKey = slKey;
+    }
 }

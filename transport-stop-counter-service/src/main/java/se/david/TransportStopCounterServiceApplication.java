4a583226c079cab2a239303eb26a44f883667660
David Jonsson <david.jonsson306@gmail.com>
2023-08-21 15:24:06 +0200

    [Service] Need to set CORS header in the response
    React client complained about cors response..

--- transport-stop-counter-service/src/main/java/se/david/TransportStopCounterServiceApplication.java
+++ transport-stop-counter-service/src/main/java/se/david/TransportStopCounterServiceApplication.java
@@ -1,14 +1,20 @@
 package se.david;
 
+import java.util.EnumSet;
+
 import se.david.cli.RunSLCommand;
 import se.david.resources.TransportsResource;
 import se.david.server.sl.ApiClient;
 import se.david.server.sl.Configuration;
 
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.FilterRegistration;
 import jakarta.ws.rs.client.Client;
 
 public class TransportStopCounterServiceApplication extends Application<TransportStopCounterServiceConfiguration>
@@ -38,11 +44,17 @@ public class TransportStopCounterServiceApplication extends Application<Transpor
                                     .build(getName());
 
         ApiClient apiClient = Configuration.getDefaultApiClient();
-		// Replace the default client with a configurable client in dropwizard (gzip: true)
+        // Replace the default client with a configurable client in dropwizard (gzip: true)
         apiClient.setHttpClient(httpClient);
         apiClient.setBasePath(configuration.getBackendUrl());
 
         TransportsResource resource = new TransportsResource(apiClient, configuration.getSlKey());
         environment.jersey().register(resource);
+
+        FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 }

11ea58402fb88952ea8e2bde65349329b4e8c4e1
David Jonsson <david.jonsson306@gmail.com>
2023-08-21 14:16:52 +0200

    [Service] Make a call to base/LineData?model=jour and log response

--- transport-stop-counter-service/src/main/java/se/david/cli/RunSLCommand.java
+++ transport-stop-counter-service/src/main/java/se/david/cli/RunSLCommand.java
@@ -1,7 +1,16 @@
 package se.david.cli;
 
+import se.david.server.sl.ApiClient;
+import se.david.server.sl.ApiException;
+import se.david.server.sl.Configuration;
+import se.david.server.sl.api.HallplatserOchLinjerApi;
+
+import io.dropwizard.client.JerseyClientBuilder;
+import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.core.cli.Command;
 import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
+import jakarta.ws.rs.client.Client;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
 
@@ -13,11 +22,28 @@ public class RunSLCommand extends Command
     public void configure(Subparser parser)
     {
         parser.addArgument("-u", "--url").dest("url").type(String.class).required(true).help("The backend URL to use");
+        parser.addArgument("-k", "--key").dest("key").type(String.class).required(true).help("The API key to use");
     }
 
     @Override
     public void run(Bootstrap<?> bootstrap, Namespace namespace) throws Exception
     {
-        System.out.println(getDescription());
+        ApiClient client = Configuration.getDefaultApiClient();
+        client.setBasePath(namespace.getString("url"));
+        JerseyClientConfiguration config     = new JerseyClientConfiguration();
+        Environment               env        = new Environment(getName());
+        Client                    httpClient = new JerseyClientBuilder(env).using(config).build(getName());
+        client.setHttpClient(httpClient);
+
+        HallplatserOchLinjerApi api = new HallplatserOchLinjerApi(client);
+        try
+        {
+            var response = api.lineDataJsonmodeljourGet(namespace.getString("key"));
+            System.out.println(response.toString());
+        }
+        catch (ApiException e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
